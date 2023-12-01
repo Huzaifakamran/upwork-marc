9e73c5f95111e84c6c2090a9dcf90d254242b274
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-12-01 12:11:37 +0500

    changes

--- app.py
+++ app.py
@@ -10,26 +10,31 @@ load_dotenv()
 client = OpenAI(api_key=st.secrets['OPENAI_API_KEY'])
 
 messages = []
-# Function to extract text from a document
-def extract_text(uploaded_file):
-    # Check if the file is a text file
-    if uploaded_file.type == "text/plain":
-        text = uploaded_file.read()
-        return text
-    elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        docx_file = Document(uploaded_file)
-        text = "\n".join([paragraph.text for paragraph in docx_file.paragraphs])
-        return text
-    elif uploaded_file.type == "application/pdf":
-        pdf_document = fitz.open(uploaded_file)
-        text = ""
-        for page_num in range(pdf_document.page_count):
-            page = pdf_document[page_num]
-            text += page.get_text()
-        return text
-    
+
+# Function to extract text from different document types
+def extract_text(uploaded_file, user_text):
+    if uploaded_file is not None:
+        if uploaded_file.type == "text/plain":
+            text = uploaded_file.read()
+        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            docx_file = Document(uploaded_file)
+            text = "\n".join([paragraph.text for paragraph in docx_file.paragraphs])
+        elif uploaded_file.type == "application/pdf":
+            pdf_document = fitz.open(uploaded_file)
+            text = ""
+            for page_num in range(pdf_document.page_count):
+                page = pdf_document[page_num]
+                text += page.get_text()
+        else:
+            st.warning("Please upload a valid text document (e.g., .txt)")
+            return None
+    elif user_text:
+        text = user_text
     else:
-        st.warning("Please upload a valid text document (e.g., .txt)")
+        st.warning("Please upload a document or enter text in the text area.")
+        return None
+
+    return text
 
 # Function to process user input and generate an answer
 def generate_answer(question):
@@ -38,11 +43,11 @@ def generate_answer(question):
     messages.append(user_dict)
 
     completion = client.chat.completions.create(
-    model="gpt-4-1106-preview",
-    messages=messages
+        model="gpt-4-1106-preview",
+        messages=messages
     )
     answer = completion.choices[0].message.content
-    sys_dict = {"role": "system", "content":answer}
+    sys_dict = {"role": "system", "content": answer}
     messages.append(sys_dict)
     return answer
 
@@ -53,7 +58,14 @@ def main():
 
         # Sidebar for document upload
         st.sidebar.header("Upload Document")
-        document = st.sidebar.file_uploader("Choose a document", type=["txt","docx" ,"pdf"])
+        document = st.sidebar.file_uploader("Choose a document", type=["txt", "docx", "pdf"])
+
+        # Text area for pasting text
+        st.sidebar.header("Or Paste Text")
+        user_text = st.sidebar.text_area("Paste your text here:")
+
+        # Submit button for pasted text
+        submit_button = st.sidebar.button("Submit")
 
         # Initialize session state
         if 'qa_history' not in st.session_state:
@@ -62,10 +74,10 @@ def main():
         # Main content area
         st.subheader("Document Text Extraction")
 
-        # Display extracted text from the document
-        if document is not None:
-            text = extract_text(document)
+        # Extract text from the document or user-pasted text
+        text = extract_text(document, user_text)
 
+        if text is not None:
             sys_dict = {"role": "system", "content": text}
             messages.append(sys_dict)
 
@@ -94,9 +106,10 @@ def main():
                 st.write(f"Question: {item['question']}")
                 st.write(f"Answer: {item['answer']}")
                 st.markdown("---")
-    
+
     except Exception as e:
         st.write(e)
+
 # Run the app
 if __name__ == "__main__":
     main()
